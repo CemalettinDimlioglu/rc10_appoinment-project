bbd88353566aa4515b775af1d68a570abd8d04ba
CMLTTN <cemaldim@gmail.com>
2023-06-20 23:01:30 +0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Doctors from "../components/Doctors"
 import { doctorData } from "../helper/data"
 import { appointmentData } from "../helper/data"
+import AppointmentList from "../components/AppointmentList"
  
  const Home = () => {
 
@@ -13,6 +14,7 @@ import { appointmentData } from "../helper/data"
      <main className="text-center mt-2 vh-100" >
       <h1 className="display-5 text-danger" >DIMLY HOSPITAL</h1>
       <Doctors doctors={doctors} appointments={appointments} setAppointments={setAppointments}/>
+      <AppointmentList/>
      </main>
    )
  }
