de690e47a80630841d655969b000e21b0e3054b4
CMLTTN <cemaldim@gmail.com>
2023-06-21 13:27:49 +0300

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react"
-import Doctors from "../components/Doctors"
-import { doctorData } from "../helper/data"
-import { appointmentData } from "../helper/data"
-import AppointmentList from "../components/AppointmentList"
- 
+ import { useState } from "react";
+ import Doctors from "../components/Doctors";
+ import { doctorData } from "../helper/data";
+ import { appointmentData } from "../helper/data";
+ import AppointmentList from "../components/AppointmentList";
  const Home = () => {
-
-   const [doctors, setDoctors] = useState(doctorData)
-   const [appointments, setAppointments] = useState(appointmentData)
-   console.log(doctors);
+   const [doctors, setDoctors] = useState(doctorData);
+   const [appointments, setAppointments] = useState(appointmentData);
    console.log(appointments);
+   console.log(doctors);
    return (
-     <main className="text-center mt-2 vh-100" >
-      <h1 className="display-5 text-danger" >DIMLY HOSPITAL</h1>
-      <Doctors doctors={doctors} appointments={appointments} setAppointments={setAppointments}/>
-      <AppointmentList/>
+     <main className="text-center mt-2 vh-100">
+       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+       <Doctors
+         doctors={doctors}
+         appointments={appointments}
+         setAppointments={setAppointments}
+       />
+       <AppointmentList
+         appointments={appointments}
+         setAppointments={setAppointments}
+       />
      </main>
-   )
- }
- 
+   );
+ };
  export default Home;
+
+
+
+
+
+
+
